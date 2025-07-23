67383aa1db1970a27987c26697823ba5bd7d6bb0
Seohyun Lee <applemint2003@gmail.com>
2025-07-23 16:47:21 +0900

    feat: PWA 코드 추가

--- bookduck/src/main.jsx
+++ bookduck/src/main.jsx
@@ -21,3 +21,15 @@ createRoot(document.getElementById("root")).render(
     </QueryClientProvider>
   </React.Suspense>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
